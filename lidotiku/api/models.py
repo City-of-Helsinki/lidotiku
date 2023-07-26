19cf394f1ccfd260466f4feb1697850cccce2bed
Henri Nieminen <henri.nieminen@nordcloud.com>
2023-07-26 11:35:58 +0300

    Fix models according to real schema, test db

--- lidotiku/api/models.py
+++ lidotiku/api/models.py
@@ -1,32 +1,35 @@
 from django.db import models
 
 class EcoCounterObservation(models.Model):
-    # class Meta:
-    #     managed = False
-        
+    class Meta:
+        managed = False
+        db_table = 'ecocounter_observations'
+    
+    
     # ID alone is not primary key    
-    id = models.IntegerField(primary_key=True)
-    direction = models.CharField(max_length=255)
-    value = models.IntegerField(blank=True)
-    unit = models.CharField(max_length=255)
-    typeofmeasurement = models.CharField(max_length=255)
-    phenomenondurationseconds = models.IntegerField(blank=False)
-    vehicletype = models.CharField(max_length=255) 
-    datetime = models.DateTimeField(max_length=255)
-    source = models.CharField(max_length=255)
+    id = models.BigIntegerField(primary_key=True)
+    direction = models.CharField(max_length=32)
+    value = models.BigIntegerField(blank=True)
+    unit = models.CharField(max_length=8)
+    typeofmeasurement = models.CharField(max_length=32)
+    phenomenondurationseconds = models.BigIntegerField(blank=False)
+    vehicletype = models.CharField(max_length=32) 
+    datetime = models.DateTimeField()
+    source = models.CharField(max_length=32)
 
 class EcoCounterCounter(models.Model):
-    # class Meta:
-    #     managed = False
+    class Meta:
+        managed = False
+        db_table = 'ecocounter_counters'
 
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-    classifying = models.CharField(max_length=255)
-    longitude = models.CharField(max_length=255)
-    latitude = models.CharField(max_length=255)
-    crs_epsg = models.IntegerField()
-    source = models.CharField(max_length=255)
-    geom = models.CharField(max_length=255)
+    name = models.CharField(max_length=32)
+    classifying = models.BooleanField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    crs_epsg = models.BigIntegerField()
+    source = models.CharField(max_length=32)
+    geom = models.CharField(max_length=255) # TODO: convert later to correct type
     
 
 
