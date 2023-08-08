6de77b3df296ffda70744e4229b48bba6282778c
Henri Nieminen <henri.nieminen@nordcloud.com>
2023-08-08 13:25:49 +0300

    Adds endpoint /counters/{id}/data

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,24 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework_nested import routers
 from . import views
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(
-    r"counters/data", views.CountersWithLatestObservationsView, basename="counters_data"
+    r"counters/data",
+    views.CountersWithLatestObservationsView,
+    basename="counters-data",
 )
+
 router.register(r"counters", views.CounterViewSet)
+data_router = routers.NestedSimpleRouter(router, r"counters", lookup="counter")
+data_router.register(
+    r"data", views.CounterWithLatestObservationsView, basename="counter-data"
+)
 router.register(r"observations", views.ObservationViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(data_router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- api/views.py
+++ api/views.py
@@ -6,7 +6,12 @@ from django.db.models.query import QuerySet
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Counter, Observation, CounterWithLatestObservations
-from .serializers import CounterSerializer, ObservationSerializer, CounterDataSerializer
+from .serializers import (
+    CounterSerializer,
+    ObservationSerializer,
+    CounterDataSerializer,
+    CountersValuesSerializer,
+)
 
 
 class CounterViewSet(viewsets.ModelViewSet):
@@ -29,12 +34,6 @@ class ObservationViewSet(viewsets.ModelViewSet):
     # permission_classes = [permissions.IsAuthenticated]
 
 
-class CountersData:
-    def __init__(self, data_updated_time, stations):
-        self.data_updated_time = data_updated_time
-        self.stations = stations
-
-
 @dataclass
 class ObservationData:
     id: int
@@ -54,43 +53,54 @@ class CountersWithObservations:
     stations: QuerySet[CounterWithLatestObservations]
 
 
+def _group_counter_sensors_in_qs(queryset):
+    counter_groups = []
+    # Group counters (sensors) due to LEFT JOIN in query with sensors returning multiple
+    for _k, g in groupby(queryset):
+        counter_groups.append(list(g))
+
+    counters = []
+    # Merges multiple counter rows by putting "duplicate" sensor rows in sensor_values
+    for sensors in counter_groups:
+        counter = sensors[0]
+        duration_delta = timedelta(
+            seconds=getattr(counter, "phenomenondurationseconds", 0)
+        )
+        measured_time = getattr(counter, "measured_time")
+        time_window_start = (measured_time - duration_delta) if measured_time else None
+        counter.sensor_values: List[ObservationData] = [
+            ObservationData(
+                id=sensor.id,
+                station_id=sensor.id,
+                name=getattr(sensor, "measurement_type"),
+                short_name=getattr(sensor, "short_name"),
+                time_window_start=time_window_start,
+                time_window_end=measured_time,
+                measured_time=measured_time,
+                value=getattr(sensor, "value"),
+                unit=getattr(sensor, "unit"),
+            )
+            for sensor in sensors
+        ]
+        counter.tms_number = counter.id
+        counter.data_updated_time = counter.counter_updated_at
+        counters.append(counter)
+    return counters
+
+
+class CounterWithLatestObservationsView(viewsets.ViewSet):
+    def list(self, request, pk=None, counter_pk=None):
+        queryset = CounterWithLatestObservations.objects.filter(id=counter_pk)
+        counter = _group_counter_sensors_in_qs(queryset)[0]
+        serializer = CountersValuesSerializer(counter)
+        return Response(serializer.data)
+
+
 class CountersWithLatestObservationsView(viewsets.ViewSet):
     def list(self, request):
         # itertools.groupby() requires sorted iterable
         queryset = CounterWithLatestObservations.objects.all().order_by("id")
-        counter_groups = []
-        # Group counters (sensors) due to LEFT JOIN in query with sensors returning multiple
-        for _k, g in groupby(queryset):
-            counter_groups.append(list(g))
-
-        counters = []
-        # Merges multiple counter rows by putting "duplicate" sensor rows in sensor_values
-        for sensors in counter_groups:
-            counter = sensors[0]
-            duration_delta = timedelta(
-                seconds=getattr(counter, "phenomenondurationseconds", 0)
-            )
-            measured_time = getattr(counter, "measured_time")
-            time_window_start = (
-                (measured_time - duration_delta) if measured_time else None
-            )
-            counter.sensor_values: List[ObservationData] = [
-                ObservationData(
-                    id=sensor.id,
-                    station_id=sensor.id,
-                    name=getattr(sensor, "measurement_type"),
-                    short_name=getattr(sensor, "short_name"),
-                    time_window_start=time_window_start,
-                    time_window_end=measured_time,
-                    measured_time=measured_time,
-                    value=getattr(sensor, "value"),
-                    unit=getattr(sensor, "unit"),
-                )
-                for sensor in sensors
-            ]
-            counter.tms_number = counter.id
-            counter.data_updated_time = counter.counter_updated_at
-            counters.append(counter)
+        counters = _group_counter_sensors_in_qs(queryset)
 
         latest_updated_at = getattr(
             Observation.objects.latest("datetime"),
