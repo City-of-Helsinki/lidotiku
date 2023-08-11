5063dba8f6072d3fdf105538a0c44f11c642c8f8
Henri Nieminen <henri.nieminen@nordcloud.com>
2023-08-11 13:05:13 +0300

    Reorganize import order

--- lidotiku/urls.py
+++ lidotiku/urls.py
@@ -1,4 +1,3 @@
-from . import views
 """
 URL configuration for lidotiku project.
 
@@ -17,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("api/", include("api.urls")),
