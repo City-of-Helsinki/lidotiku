10f37d3e551088fe9fe75f681594bfff6f118479
Henri Nieminen <henri.nieminen@nordcloud.com>
2023-08-11 14:11:31 +0300

    Adds requirements-dev.txt and mypy.ini, stubs

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,4 +1,5 @@
 pip install --user -r requirements.txt &&
+pip install --user -r requirements-dev.txt &&
 # pip install --user -r requirements-dev.txt &&
 PGSERVICEFILE=".pgservice.conf" ./manage.py migrate
 ### If you prefer not to use django runserver
