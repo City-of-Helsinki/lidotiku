366fb3f3f06787ccac0641a1ccbee0a16ce97cce
Henri Nieminen <henri.nieminen@nordcloud.com>
2023-08-24 12:19:34 +0000

    Adds schema for geometry field

--- api/schemas.py
+++ api/schemas.py
@@ -76,6 +76,23 @@ class CounterSchema(BaseSchema):
             )["schema"] = GEOJSON_POLYGON_JSONSCHEMA
         return operation
 
+    def map_field(self, field):
+        if field.field_name == "geometry":
+            return {
+                "type": "object",
+                "properties": {
+                    "type": {"type": "string", "enum": ["Point"]},
+                    "coordinates": {
+                        "type": "array",
+                        "minItems": 2,
+                        "maxItems": 2,
+                        "items": {"type": "number", "format": "float"},
+                    },
+                },
+            }
+
+        return super().map_field(field)
+
 
 class ObservationSchema(BaseSchema):
     def get_operation(self, path, method):
