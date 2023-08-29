b871c8a482b68c9cfac63a360050b78651d20113
Henri Nieminen <henri.nieminen@nordcloud.com>
2023-08-29 10:45:36 +0000

    Uses gdal-bin instead of libgdal

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,6 +1,6 @@
 pip install --user -r requirements.txt &&
-pip install --user -r requirements-dev.txt &&
-./manage.py migrate
+pip install --user -r requirements-dev.txt
+#./manage.py migrate
 ### If you prefer not to use django runserver
 # pip install --user gunicorn
 # gunicorn --bind 0.0.0.0:8000 lidotiku.wsgi --reload
