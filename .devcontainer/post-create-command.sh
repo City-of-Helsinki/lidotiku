a383c2c9424c8baffe81e2c851227d1f56fe012e
Mika Hietanen <m.hietanen84@gmail.com>
2026-08-06 08:57:38 +0300

    build(devcontainer): use uv instead of pip
    Refs: LT-320

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-pip install --user -r requirements.txt &&
-pip install --user -r requirements-dev.txt
-#./manage.py migrate
+uv sync --locked --group dev
+# uv run manage.py migrate
 ### If you prefer not to use django runserver
-# pip install --user gunicorn
-# gunicorn --bind 0.0.0.0:8000 lidotiku.wsgi --reload
+# uv run --group prod gunicorn --bind 0.0.0.0:8000 lidotiku.wsgi --reload
